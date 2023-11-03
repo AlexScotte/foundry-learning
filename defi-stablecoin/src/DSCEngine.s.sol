7dcb895e4c7b57ca0f3c3fadcfa6cf888a8c3c44
AlexScotte <scottealexandre@gmail.com>
2023-11-03 15:19:15 +0100

    [lesson 12] deposit collateral

--- defi-stablecoin/src/DSCEngine.s.sol
+++ defi-stablecoin/src/DSCEngine.s.sol
@@ -25,7 +25,10 @@
 pragma solidity ^0.8.19;
 
 import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+// import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {DecentralizedStableCoin} from "./DecentralizedStableCoin.s.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /**
  * @title DSCEngine
@@ -45,10 +48,90 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
  * as well as depositing & withdrawing collateral
  * @notice This contract is VERY loosely based on the MakerDAO DSS (DAI) system.
  */
-contract DSCEngine is Ownable {
+contract DSCEngine is ReentrancyGuard {
+    ////////////
+    // ERRORS //
+    ////////////
+    error DSCEngine_NeedsMoreThanZero();
+    error DSCEngine_TokenAddressesAndPriceFeedAddressesMustBeSameLength();
+    error DSCEngine_NotAllowedToken();
+    error DSCEngine_CollateralTransferFailed();
+
+    /////////////////////
+    // STATE VARIABLES //
+    /////////////////////
+    mapping(address token => address priceFeed) public s_priceFeeds; // tokenToPriceFeed
+    mapping(address user => mapping(address token => uint256 amount)) private s_collateralDeposited;
+
+    DecentralizedStableCoin private immutable i_dsc;
+
+    ////////////
+    // EVENTS //
+    ////////////
+
+    event CollateralDeposited(address indexed user, address indexed token, uint256 amount);
+
+    /////////////////
+    // CONSTRUCTOR //
+    /////////////////
+
+    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address dscAddress) {
+        // USD Price Feeds
+        if (tokenAddresses.length != priceFeedAddresses.length) {
+            revert DSCEngine_TokenAddressesAndPriceFeedAddressesMustBeSameLength();
+        }
+
+        // For example ETH/USD, BTC/USD, MKR/USD, etc
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+        }
+
+        i_dsc = DecentralizedStableCoin(dscAddress);
+    }
+
+    ///////////////
+    // MODIFIERS //
+    ///////////////
+
+    modifier moreThanZero(uint256 amount) {
+        if (amount == 0) {
+            revert DSCEngine_NeedsMoreThanZero();
+        }
+        _;
+    }
+
+    modifier isAllowedToken(address tokenAddress) {
+        if (s_priceFeeds[tokenAddress] == address(0)) {
+            revert DSCEngine_NotAllowedToken();
+        }
+        _;
+    }
+
+    ////////////////////////
+    // EXTERNAL FUNCTIONS //
+    ////////////////////////
+
     function depositCollateralAndMintDsc() external {}
 
-    function depositCollateral() external {}
+    /**
+     *
+     * @param tokenCollateralAddress the address of the token to deposit as collateral
+     * @param amountCollateral  the amount of collateral to deposit
+     */
+    function depositCollateral(address tokenCollateralAddress, uint256 amountCollateral)
+        external
+        moreThanZero(amountCollateral)
+        isAllowedToken(tokenCollateralAddress)
+        nonReentrant
+    {
+        s_collateralDeposited[msg.sender][tokenCollateralAddress] += amountCollateral;
+        emit CollateralDeposited(msg.sender, tokenCollateralAddress, amountCollateral);
+
+        bool success = IERC20(tokenCollateralAddress).transferFrom(msg.sender, address(this), amountCollateral);
+        if (!success) {
+            revert DSCEngine_CollateralTransferFailed();
+        }
+    }
 
     function redeemCollateralForDsc() external {}
 
