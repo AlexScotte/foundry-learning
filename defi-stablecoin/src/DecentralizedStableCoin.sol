ada255b2fea15c3d3fddcdf4d576f239591c22fd
AlexScotte <scottealexandre@gmail.com>
2023-11-03 10:15:50 +0100

    [lesson 12] create project

---
+++ defi-stablecoin/src/DecentralizedStableCoin.sol
@@ -0,0 +1,40 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+/**
+ * @title DeecentralizedStableCoin
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to USD
+ *
+ * This is the contract meant to be governed by DSCEngine.
+ * This contract is just the ERC20 implementation of our stablecoin system.
+ */
+contract DecentralizedStableCoin {
+    constructor() {}
+}
